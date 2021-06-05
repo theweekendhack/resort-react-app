97a38ad5e2c5e68d83ef45e37116d7c44b5b48a4
Kadeem Best <info@theweekendhack.com>
2021-06-04 20:58:47 -0400

    Start Coder

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+import "../assets/css/App.css"
+
+const App = () => {
+  return (
+    <div>
+        <h1>Kadeem Best</h1>
+    </div>
+  )
+}
+
+export default App
+
